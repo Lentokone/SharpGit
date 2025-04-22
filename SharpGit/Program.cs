f82935fff8f6a1a887e52f7516bd3bdd66e83c19
Lentokone <olli22012@student.hamk.fi>
2025-04-22 13:40:47 +0300

    Console side project init

---
+++ SharpGit/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// Eli se libgit2sharp
+// Ja ssh
+// Ja auth tarkistus mvc puolelle
+
+// Kun on pushaamassa, lähettää ennen sitä jonkun viestin mvc puolelle, joka tekee sen git init --bare puolen
+
+// Vaikka git remote add kohdassa voisi tehdä sen auth jutun ja tag se local repo jotenkin että se tietää että on auth kontsa
