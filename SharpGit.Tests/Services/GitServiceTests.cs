bd56e1250411a4fda25d290cdcc5569b64ce2765
Lentokone <olli22012@student.hamk.fi>
2026-03-14 14:32:18 +0200

    Feat: Add more tests
    Tests for 'AddToRepo()', 'AddToRepoUpdate()', 'AddToRepoAll()', should be done now

--- SharpGit.Tests/Services/GitServiceTests.cs
+++ SharpGit.Tests/Services/GitServiceTests.cs
@@ -9,7 +9,7 @@ public class GitServiceTests
 	// Existing file but outside of Repository
 	// Adding same file twice and checking only one instance of it appears
 	[Fact]
-	public void TestAddToRepo()
+	public void AddToRepo_AddsAFile()
 	{
 		var TestingPath = Path.Combine(Path.GetTempPath(), "SharpGitTests-" + Guid.NewGuid());
 		try
@@ -36,7 +36,7 @@ public class GitServiceTests
 	}
 
 	[Fact]
-	public void TestAddNonExistentToRepo()
+	public void AddToRepo_DoesNotAddNonExistent()
 	{
 
 		var TestingPath = Path.Combine(Path.GetTempPath(), "SharpGitTests-" + Guid.NewGuid());
@@ -59,7 +59,7 @@ public class GitServiceTests
 	}
 
 	[Fact]
-	public void TestAddFileFromOutsideRepo()
+	public void AddToRepo_WontAddFileFromOutsideRepo()
 	{
 		var TestingPath = Path.Combine(Path.GetTempPath(), "SharpGitTests-" + Guid.NewGuid());
 		try
@@ -92,7 +92,7 @@ public class GitServiceTests
 	///
 	/// </summary>
 	[Fact]
-	public void TestAddDuplicateToRepo()
+	public void AddToRepo_DoesNotAddDuplicate()
 	{
 		var TestingPath = Path.Combine(Path.GetTempPath(), "SharpGitTests-" + Guid.NewGuid());
 		try
@@ -124,30 +124,167 @@ public class GitServiceTests
 	}
 
 	[Fact]
-	public static void AddToRepoUpdate_ShouldAddTracked()
+	public static void AddToRepoUpdate_AddTracked()
 	{
+		var TestingPath = Path.Combine(Path.GetTempPath(), "SharpGitTests-" + Guid.NewGuid());
+		try
+		{
+			var RepoPath = Path.Combine(TestingPath, "test");
+			string RootedPath = Repository.Init(RepoPath);
+			Assert.True(Directory.Exists(Path.Combine(RootedPath, "objects")));
 
+			var repo = new Repository(RootedPath);
+			Assert.Empty(repo.RetrieveStatus());
+
+			string file1path = Path.Combine(RepoPath, "test1.txt");
+			File.WriteAllText(file1path, "Test 1");
+			var status = repo.RetrieveStatus();
+			Assert.Single(status);
+			Assert.Equal("test1.txt", status.First().FilePath);
+
+			repo.Index.Add("test1.txt");
+			repo.Index.Write();
+			Signature author = new Signature("James", "@jugglingnutcase", DateTime.Now);
+			Signature committer = author;
+
+			Commit commit = repo.Commit("Here's a commit i made!", author, committer);
+			status = repo.RetrieveStatus();
+			Assert.Empty(status);
+			File.WriteAllText(file1path, "Beast of burden");
+			GitService.AddToRepoUpdate(repo);
+			status = repo.RetrieveStatus();
+			Assert.Single(status);
+			Assert.Equal("test1.txt", status.First().FilePath);
+		}
+		finally
+		{
+			if (Directory.Exists(TestingPath))
+				Directory.Delete(TestingPath, true);
+		}
 	}
 
 	[Fact]
-	public static void AddToRepoUpdate_ShouldAddOnlyTracked()
+	public static void AddToRepoUpdate_AddOnlyTracked()
 	{
+		var TestingPath = Path.Combine(Path.GetTempPath(), "SharpGitTests-" + Guid.NewGuid());
+		try
+		{
+			var RepoPath = Path.Combine(TestingPath, "test");
+			string RootedPath = Repository.Init(RepoPath);
+			Assert.True(Directory.Exists(Path.Combine(RootedPath, "objects")));
 
+			var repo = new Repository(RootedPath);
+			Assert.Empty(repo.RetrieveStatus());
+
+			string file1path = Path.Combine(RepoPath, "test1.txt");
+			File.WriteAllText(file1path, "Test 1");
+			string file2path = Path.Combine(RepoPath, "test2.txt");
+			File.WriteAllText(file2path, "Test 2");
+			var status = repo.RetrieveStatus();
+			Assert.Equal(2, status.Count());
+			Assert.All(status, s => Assert.Equal(LibGit2Sharp.FileStatus.NewInWorkdir, s.State));
+			repo.Index.Add("test1.txt");
+			repo.Index.Write();
+
+			var filtered = repo.RetrieveStatus()
+			    .Where(i =>
+				(i.State & FileStatus.NewInIndex) != 0);
+			Assert.Single(filtered);
+			File.WriteAllText(file1path, "Beast of Burden");
+			File.WriteAllText(file2path, "BoB");
+
+			GitService.AddToRepoUpdate(repo);
+
+			foreach (var entry in repo.RetrieveStatus())
+			{
+				if (entry.FilePath == "test1.txt")
+				{
+					Assert.NotEqual(FileStatus.ModifiedInWorkdir, entry.State);
+				}
+				else
+				{
+					Assert.Equal(FileStatus.NewInWorkdir, entry.State);
+				}
+			}
+		}
+		finally
+		{
+			if (Directory.Exists(TestingPath))
+				Directory.Delete(TestingPath, true);
+		}
 	}
 
 	[Fact]
-	public static void AddToRepoUpdate_ShouldNotAddDuplicate()
+	public static void AddToRepoUpdate_DoesNotAddDuplicate()
 	{
+		var TestingPath = Path.Combine(Path.GetTempPath(), "SharpGitTests-" + Guid.NewGuid());
+		try
+		{
+			var RepoPath = Path.Combine(TestingPath, "test");
+			string RootedPath = Repository.Init(RepoPath);
+			Assert.True(Directory.Exists(Path.Combine(RootedPath, "objects")));
 
+			var repo = new Repository(RootedPath);
+			Assert.Empty(repo.RetrieveStatus());
+
+			string file1path = Path.Combine(RepoPath, "test1.txt");
+			File.WriteAllText(file1path, "Test 1");
+			var status = repo.RetrieveStatus();
+			Assert.Single(status);
+			Assert.Equal("test1.txt", status.First().FilePath);
+
+			repo.Index.Add("test1.txt");
+			repo.Index.Write();
+			Signature author = new Signature("James", "@jugglingnutcase", DateTime.Now);
+			Signature committer = author;
+
+			Commit commit = repo.Commit("Here's a commit i made!", author, committer);
+			status = repo.RetrieveStatus();
+			Assert.Empty(status);
+			File.WriteAllText(file1path, "Beast of burden");
+			GitService.AddToRepoUpdate(repo);
+			status = repo.RetrieveStatus();
+			Assert.Single(status);
+			Assert.Equal("test1.txt", status.First().FilePath);
+
+			GitService.AddToRepoUpdate(repo);
+			status = repo.RetrieveStatus();
+			Assert.Single(status);
+			Assert.Equal("test1.txt", status.First().FilePath);
+
+		}
+		finally
+		{
+			if (Directory.Exists(TestingPath))
+				Directory.Delete(TestingPath, true);
+		}
 	}
 
 	[Fact]
-	public static void AddToRepoUpdate_ShouldNotAddWhenEmpty()
+	public static void AddToRepoUpdate_WillNotAddWhenEmpty()
 	{
+		var TestingPath = Path.Combine(Path.GetTempPath(), "SharpGitTests-" + Guid.NewGuid());
+		try
+		{
+			var RepoPath = Path.Combine(TestingPath, "test");
+			string RootedPath = Repository.Init(RepoPath);
+			Assert.True(Directory.Exists(Path.Combine(RootedPath, "objects")));
 
+			var repo = new Repository(RootedPath);
+			Assert.Empty(repo.RetrieveStatus());
+
+			GitService.AddToRepoUpdate(repo);
+			Assert.Empty(repo.RetrieveStatus());
+		}
+		finally
+		{
+			if (Directory.Exists(TestingPath))
+				Directory.Delete(TestingPath, true);
+		}
 	}
+
 	[Fact]
-	public static void AddToRepoAll_ShouldAddEverything()
+	public static void AddToRepoAll_AddEverything()
 	{
 		var TestingPath = Path.Combine(Path.GetTempPath(), "SharpGitTests-" + Guid.NewGuid());
 		try
@@ -181,7 +318,7 @@ public class GitServiceTests
 	}
 
 	[Fact]
-	public static void AddToRepoAll_ShouldNotAddAnythingInAnEmptyRepo()
+	public static void AddToRepoAll_DoesNotAddAnythingInAnEmptyRepo()
 	{
 		var TestingPath = Path.Combine(Path.GetTempPath(), "SharpGitTests-" + Guid.NewGuid());
 		try
@@ -204,7 +341,7 @@ public class GitServiceTests
 	}
 
 	[Fact]
-	public static void AddToRepoAll_ShouldNotAddDuplicateIfCalledTwice()
+	public static void AddToRepoAll_DoesNotAddDuplicateIfCalledTwice()
 	{
 		var TestingPath = Path.Combine(Path.GetTempPath(), "SharpGitTests-" + Guid.NewGuid());
 		try
